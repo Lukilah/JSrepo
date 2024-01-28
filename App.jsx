faba88660f84347f0d00ab15a4dbf2d44efa15c5
Lukilah <111893154+Lukilah@users.noreply.github.com>
2024-01-29 00:23:56 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import JokesComponent from './component/JokesComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chuck Norris Jokes App</h1>
+      <JokesComponent />
+    </div>
+  );
+}
